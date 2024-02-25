63bd979599108e2149cf600d333f3bb50e0e31b3
MariaShopEveliko <maria.shop@eveliko.com>
2024-02-26 00:25:11 +0200

    Card header

--- src/components/pages/UserDashboard.vue
+++ src/components/pages/UserDashboard.vue
@@ -1,29 +1,50 @@
 <template>
     <section class="profile-card">
-        <div class="profile-card-header d-flex flex-column">
-            <div class="d-flex align-items-center profile-info-wrp">
-                <div class="profile-img" :style="{ 'background-image': 'url(' + require('@/assets/img/avatar.png') + ')' }">
+        <div class="profile-card-header">
+            <div class="profile-info-wrp d-flex flex-column">
+                <div class="d-flex align-items-center">
+                    <div class="profile-img"
+                        :style="{ 'background-image': 'url(' + require('@/assets/img/avatar.png') + ')' }">
+                    </div>
+                    <h2 class="text-white m-0 fs-lg profil-name">RichyRich</h2>
                 </div>
-                <h2 class="text-white m-0 fs-lg profil-name">RichyRich</h2>
+                <div class="profile-level-wrp text-center">
+                    <span class="profile-level fs-xxs text-white">Level 5</span>
+                </div>
             </div>
-            <div class="profile-level-wrp text-center">
-                <span class="profile-level fs-xxs text-white">Level 5</span>
+            <ul class="profile-tabs d-flex justify-content-center text-uppercase fs-xs">
+                <li>
+                    <a href="#" class="active">My teams</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+            </ul>
+        </div>
+        <div class="profile-card-body">
+            <h2 class="text-uppercase">Search teams</h2>
+            <div class="input-wrp">
+                <input type="text" v-model="searchQuery" @input="search" placeholder="Search for a team" class="input" />
+                <button type="button" class="btn clear-btn" @click="clearSearch()" v-show="searchQuery">
+                    <span class="sr-only">Clear input</span>
+                </button>
+            </div>
+            <ul v-if="filteredTeams.length > 0" role="list" tabindex="0">
+                <li v-for="(team, index) in filteredTeams" :key="index" role="listitem" tabindex="-1">
+                    {{ team.name }} - {{ team.stadium }} - {{ team.leagues.join(", ") }}
+                    {{ team.id }}
+                    <button
+                        :class="['btn btn-sm text-uppercase', isFollowingTheTeam(team) ? 'btn-outlined-primary' : 'btn-primary']"
+                        @click="toggleFollow(team)">
+                        {{ isFollowingTheTeam(team) ? 'Following' : 'Follow' }}
+                    </button>
+                </li>
+            </ul>
+            <div class="text-center" v-else>
+                <img src="@/assets/img/no-results.png" />
+                <p class="text-muted m-0">No Teams Found</p>
             </div>
         </div>
-        Search teams
-        <input type="text" v-model="searchQuery" @input="search" placeholder="Search for a team" />
-        <ul role="list" tabindex="0">
-            <li v-for="(team, index) in filteredTeams" :key="index" role="listitem" tabindex="-1">
-                {{ team.name }} - {{ team.stadium }} - {{ team.leagues.join(", ") }}
-                {{ team.id }}
-                <button
-                    :class="['btn btn-sm text-uppercase', isFollowingTheTeam(team) ? 'btn-outlined-primary' : 'btn-primary']"
-                    @click="toggleFollow(team)">
-                    {{ isFollowingTheTeam(team) ? 'Following' : 'Follow' }}
-                </button>
-            </li>
-        </ul>
-        <div v-if="filteredTeams.length === 0">No Teams Found</div>
         <div class="profile-card-footer">
             <h2 class="text-uppercase">My teams</h2>
             <ul v-if="followedTeams.length > 0">
@@ -94,6 +115,10 @@ export default {
         isFollowingTheTeam(team) {
             return this.followedTeams.some(followedTeam => followedTeam.id === team.id);
         },
+        clearSearch() {
+            this.searchQuery = "";
+            this.filteredTeams = this.allTeams;
+        }
     },
     beforeMount() {
         this.clearAllTeamsIfExpired();
